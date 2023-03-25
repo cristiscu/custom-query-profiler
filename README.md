b570d69d2fe8e330a771b315e603a0a789da15fc
cristiscu <cristiscu@gmail.com>
2023-03-25 08:27:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+Snowflake Custom Query Profiler
+===============================
+
+Simple tool to connect to Snowflake and generate a HTML file with a DOT graph that can show all database object dependencies.
+
+# Database Profile File
+
+Create a **profiles_db.conf** copy of the **profiles_db_template.conf** file, and customize it with your own Snowflake connection parameters. Your top [default] profile is the active profile, considered by our tool. Below you may define other personal profiles, that you may override under [default] each time you want to change your active connection.
+
+The database and schema are optional:
+* When connecting with no database and no schema, the tool will get all the data through the new **[OBJECT_DEPENDENCIES](https://docs.snowflake.com/en/user-guide/object-dependencies.html)** view.
+* When connecting with a database, the tool will look only for referenced and referencing objects from this database alone, and will skip the database name in the fully-qualified names of the nodes.
+* When connecting with both a database and a schema, the tool will look only for referenced and referencing objects from this database schema alone, and will skip the database and schema names in the fully-qualified names of the nodes.
+
+We connect to Snowflake with the Snowflake Connector for Python. We have code for (a) password-based connection, (b) connecting with a Key Pair, and (c) connecting with SSO. For password-based connection, save your password in a SNOWFLAKE_PASSWORD local environment variable. Never add the password or any other sensitive information to your code or to profile files. All names must be case sensitive, with no quotes.
+
+# CLI Executable File
+
+To compile into a CLI executable:
+
+**<code>pip install pyinstaller</code>**  
+**<code>pyinstaller --onefile custom-query-profiler.py</code>**  
+**<code>dist/custom-query-profiler</code>**  
+
+# 1. Show All Dependencies from the Current Account
+
+Connect with no database and no schema, to show a generated SVG graph in a HTML file for all existing dependencies. Call the tool as below:
+
+**<code>python custom-query-profiler.py</code>**  
+
+The following is what I had from my own Snowflake test account:
+
+![All Dependencies](/images/account.png)
+
+All displayed names are fully-qualified, and the rendering if left-to-right, with the objects that depend on other objects on the left. Dotted arrows are for BY_ID dependencies, dashed for BY_NAME, and solid for BY_NAME_AND_ID. We do not show the ID values.
+
+# 2. Show All Dependencies from a Database
+
+Connect with a database name in the profile file, than call the tool exactly like before. The following is what I had connecting with the EmployeesQX database:
+
+![Database Dependencies](/images/account-EmployeesQX.png)
+
+All displayed names are fully-qualified, but with no database name. Referenced or referencing objects from other databases are not included.
+
+# 3. Show All Dependencies from a Database Schema
+
+Connect with both a database and a schema name in the profile file, than call the tool exactly like before. The following is what I had after connecting with the PUBLIC schema from the EmployeesQX database:
+
+![Schema Dependencies](/images/account-EmployeesQX.PUBLIC.png)
+
+The displayed names are no longer fully-qualified, because all objects belong to the same database and schema. Referenced or referencing objects from other databases and schemas are not included.
+
+# 4. Show All Dependencies of a Database Object
+
+Connect with both a database and a schema name in the profile file, than call the tool with the simple name of the object. The object name must be case sensitive as well, with no quotes:
+
+**<code>python custom-query-profiler.py checkManagerEmployee</code>**  
+
+The following is what I had for the EmployeesQX.PUBLIC.checkManagerEmployee user-defined function displayed before
+
+![Object Dependencies](/images/account-EmployeesQX.PUBLIC.checkManagerEmployee.png)
+
+All displayed names are fully-qualified, in case you may have inter-database dependencies. You might have referenced or referencing objects from other databases and schemas. The rendering if now top-down, with your named object on top.
+
+# 5. Show All Dependencies on a Database Object
+
+Just like before, connect with both a database and a schema name in the profile file, than call the tool with the simple name of the object. The object name must be case sensitive as well, with no quotes. This time however append a **--reverse** option, which will show the reverse dependency hierarchy of an object:
+
+**<code>python custom-query-profiler.py emp --reverse</code>**  
+
+The following is what I had for the EmployeesQX.PUBLIC.emp table displayed before:
+
+![Object Dependencies](/images/account-EmployeesQX.PUBLIC.emp-rev.png)
+
+All displayed names are fully-qualified, in case you may have inter-database dependencies. You might have referenced or referencing objects from other databases and schemas. The rendering if also top-down, with your named object on top. All the arrows point now backwards, to the top, because the top object is a dependent object.
---
+++ custom-query-profiler.py
@@ -0,0 +1,176 @@
+"""
+Created By:    Cristian Scutaru
+Creation Date: Mar 2023
+Company:       XtractPro Software
+"""
+
+import os, sys, json
+from html import escape
+import configparser
+import snowflake.connector
+from pathlib import Path
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+def getDot(objects, cur, queryId):
+    """
+    generates and returns a graph in DOT notation
+    """
+
+    nodes = ""; edges = ""
+    query = f"select * from table(GET_QUERY_OPERATOR_STATS('{queryId}'))"
+    results = cur.execute(query).fetchall()
+    for row in results:
+        nodeId = str(row[2])
+        parentId = row[3]
+        step = str(row[4])
+        nodes += (f'  n{nodeId} [\n'
+            + f'    style="filled" shape="record" color="SkyBlue"\n'
+            + f'    fillcolor="#d3dcef:#ffffff" color="#716f64" penwidth="1"\n'
+            + f'    label=<<table style="rounded" border="0" cellborder="0" cellspacing="0" cellpadding="1">\n'
+            + f'      <tr><td bgcolor="transparent" align="center"><font color="#000000"><b>{step}</b></font></td></tr>\n')
+
+        oper = json.loads(str(row[7]))
+        if 'table_name' in oper:
+            nodes += f'      <tr><td align="left"><font color="#000000">table_name: {oper["table_name"]}</font></td></tr>\n'
+        #if 'filter_condition' in oper:
+        #    nodes += f'      <tr><td align="left"><font color="#000000">filter_condition: {escape(oper["filter_condition"])}</font></td></tr>\n'
+        if 'join_type' in oper:
+            nodes += f'      <tr><td align="left"><font color="#000000">join_type: {oper["join_type"]}</font></td></tr>\n'
+        if 'join_id' in oper:
+            edges += f'  n{nodeId} -> n{oper["join_id"]} [  dir="forward" style="dashed" ];\n'
+
+        execTime = json.loads(str(row[6]))
+        if 'overall_percentage' in execTime:
+            overall_percentage = float(execTime['overall_percentage'])
+            if overall_percentage  > 0.0:
+                nodes += f'      <tr><td align="left"><font color="#000000">overall_percentage: {"{0:.1%}".format(overall_percentage)}</font></td></tr>\n'
+            if 'remote_disk_io' in execTime:
+                nodes += f'      <tr><td align="left"><font color="#000000">remote_disk_io: {"{0:.0%}".format(execTime["remote_disk_io"])}</font></td></tr>\n'
+
+        stats = json.loads(str(row[5]))
+        if 'io' in stats:
+            io = stats["io"]
+            if 'bytes_scanned' in io:
+                nodes += f'      <tr><td align="left"><font color="#000000">bytes_scanned: {io["bytes_scanned"]:,}</font></td></tr>\n'
+            if 'percentage_scanned_from_cache' in io:
+                nodes += f'      <tr><td align="left"><font color="#000000">percentage_scanned_from_cache: {"{0:.2%}".format(io["percentage_scanned_from_cache"])}</font></td></tr>\n'
+            if 'bytes_written_to_result' in io:
+                nodes += f'      <tr><td align="left"><font color="#000000">bytes_written_to_result: {io["bytes_written_to_result"]:,}</font></td></tr>\n'
+
+        if 'pruning' in stats:
+            nodes += f'      <tr><td align="left"><font color="#000000">partitions_scanned: {stats["pruning"]["partitions_scanned"]}</font></td></tr>\n'
+            nodes += f'      <tr><td align="left"><font color="#000000">partitions_total: {stats["pruning"]["partitions_total"]}</font></td></tr>\n'
+
+        nodes += f'    </table>>\n  ]\n'
+
+        if 'input_rows' in stats:
+            nodes += f'  i{nodeId} [ label="{stats["input_rows"]:,}" style="filled" shape="oval" fillcolor="#ffffff" ]\n'
+            edges += f'  n{nodeId} -> i{nodeId};\n'
+
+        if parentId != None:
+            edges += f'  i{parentId} -> n{nodeId};\n'
+
+    return ('digraph G {\n'
+        + f'  graph [ rankdir="TB" bgcolor="#ffffff" ]\n'
+        + f'  edge [ penwidth="1" color="#696969" dir="back" style="solid" ]\n\n'
+        + f'{nodes}\n{edges}}}\n')
+
+def saveHtml(filename, s):
+    """
+    save in HTML file using d3-graphviz
+    https://bl.ocks.org/magjac/4acffdb3afbc4f71b448a210b5060bca
+    https://github.com/magjac/d3-graphviz#creating-a-graphviz-renderer
+    """
+    s = ('<!DOCTYPE html>\n'
+        + '<meta charset="utf-8">\n'
+        + '<body>'
+        + '<script src="https://d3js.org/d3.v5.min.js"></script>\n'
+        + '<script src="https://unpkg.com/@hpcc-js/wasm@0.3.11/dist/index.min.js"></script>\n'
+        + '<script src="https://unpkg.com/d3-graphviz@3.0.5/build/d3-graphviz.js"></script>\n'
+        + '<div id="graph" style="text-align: center;"></div>\n'
+        + '<script>\n'
+        + 'var graphviz = d3.select("#graph").graphviz()\n'
+        + '   .on("initEnd", () => { graphviz.renderDot(d3.select("#digraph").text()); });\n'
+        + '</script>\n'
+        + '<textarea id="digraph" style="display:none; height:0px;">\n'
+        + s
+        + '</textarea>\n')
+
+    print(f"Generating {filename} file...")
+    with open(filename, "w") as file:
+        file.write(s)
+
+def connect(connect_mode, account, user):
+
+    # (a) connect to Snowflake with SSO
+    if connect_mode == "SSO":
+        return snowflake.connector.connect(
+            account = account,
+            user = user,
+            authenticator = "externalbrowser"
+        )
+
+    # (b) connect to Snowflake with username/password
+    if connect_mode == "PWD":
+        return snowflake.connector.connect(
+            account = account,
+            user = user,
+            password = os.getenv('SNOWFLAKE_PASSWORD')
+        )
+
+    # (c) connect to Snowflake with key-pair
+    if connect_mode == "KEY-PAIR":
+        with open(f"{str(Path.home())}/.ssh/id_rsa_snowflake_demo", "rb") as key:
+            p_key= serialization.load_pem_private_key(
+                key.read(),
+                password = None, # os.environ['SNOWFLAKE_PASSPHRASE'].encode(),
+                backend = default_backend()
+            )
+        pkb = p_key.private_bytes(
+            encoding = serialization.Encoding.DER,
+            format = serialization.PrivateFormat.PKCS8,
+            encryption_algorithm = serialization.NoEncryption())
+
+        return snowflake.connector.connect(
+            account = account,
+            user = user,
+            private_key = pkb
+        )
+
+def main():
+    """
+    Main entry point of the CLI
+    """
+
+    # read profiles_db.conf
+    parser = configparser.ConfigParser()
+    parser.read("profiles_db.conf")
+    section = "default"
+    account = parser.get(section, "account")
+    user = parser.get(section, "user")
+
+    # Query ID must be passed in the command line
+    if len(sys.argv) < 2:
+        print("You must pass a valid Query ID in the command line!")
+        sys.exit(2)
+    queryId = sys.argv[1]
+
+    # change this to connect in a different way: SSO / PWD / KEY-PAIR
+    connect_mode = "PWD"
+    con = connect(connect_mode, account, user)
+    cur = con.cursor()
+
+    # get DOT digraph string
+    objects = []
+    s = getDot(objects, cur, queryId)
+    print("\nGenerated DOT digraph:")
+    print(s)
+    con.close()
+
+    # save as HTML file
+    filename = f"output/{account}.html"
+    saveHtml(filename, s)
+
+if __name__ == "__main__":
+    main()
